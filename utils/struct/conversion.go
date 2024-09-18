bfe2b111e8c3060661599df638a15199932148ba
rothi <hasanrothi99@gmail.com>
2024-09-18 12:40:46 +0600

    StructToMap: fieldName as json name

--- utils/struct/conversion.go
+++ utils/struct/conversion.go
@@ -27,8 +27,17 @@ func StructToMap(data interface{}) map[string]interface{} {
 
 	t := v.Type()
 	for i := 0; i < v.NumField(); i++ {
+
+		field := t.Field(i)
 		fieldName := t.Field(i).Name
 		fieldValue := v.Field(i).Interface()
+
+		// Check if a json tag is present
+		jsonTag := field.Tag.Get("json")
+		if jsonTag != "" && jsonTag != "-" {
+			fieldName = jsonTag
+		}
+
 		result[fieldName] = fieldValue
 	}
 
