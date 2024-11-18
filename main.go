0e2e0ed34135168dc573fc03c30ec73dc5cf8c51
rothi <hasanrothi99@gmail.com>
2024-11-18 18:32:02 +0600

    add put method

--- main.go
+++ main.go
@@ -2,15 +2,8 @@ package main
 
 import (
 	"fmt"
-	httputils "github.com/HasanRothi/go-utils-pkg/utils/http"
 )
 
 func main() {
 	fmt.Println("Go Utils pkg")
-	postman := httputils.Postman{
-		Url: "https://dummyjson.com/todos/1",
-	}
-	postman.Get()
-	fmt.Println(postman.StatusCode)
-	fmt.Println(postman.Response)
 }
---
+++ utils/http/put.go
@@ -0,0 +1,31 @@
+package httputils
+
+import (
+	"github.com/nahid/gohttp"
+)
+
+func (postman *Postman) Put() {
+	if err := postman.validatePostRequest(); err != nil {
+		postman.ErrorMessage = err
+		postman.StatusCode = 400
+		return
+	}
+
+	postman.setDefaultHeaders()
+
+	//Api initializer
+	var req *gohttp.Request
+
+	//Either json or body byte
+	if postman.Body != nil {
+		req = gohttp.NewRequest().JSON(postman.Body)
+	} else if postman.BodyByte != nil {
+		req = gohttp.NewRequest().Body(postman.BodyByte)
+	}
+
+	//Final Hit
+	apiRes, err := req.Headers(postman.Headers).Query(postman.Queries).Put(postman.Url)
+
+	postman.setApiReturnData(apiRes, err)
+
+}
